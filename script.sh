996cabfcff5912b1a642cf6f1f5af3e6dfc3a05a
Pooja-Johnson <poojajohnson2002@gmail.com>
2023-12-24 14:45:03 +0000

    added script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Export the GitHub username
-export GITHUB_USERNAME="<username>"
+export GITHUB_USERNAME="Pooja-Johnson"
 
 # Export the GitHub repository name
-export GITHUB_REPOSITORY="se2001-<roll_number>"
-
+export GITHUB_REPOSITORY="se2001-22f1000937"
